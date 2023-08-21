c4dc313fbe40e1f2987bb5ae42005b44bbd20500
ShubhamTiwari55 <stofficial059@gmail.com>
2023-08-21 16:56:18 +0530

    project started

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
 import Pokedex from './components/pokedex/pokedex'
+import Search from './components/search/Search'
 
 function App() {
   return (
+  <> <div className='pokedex-wrapper'>
     <Pokedex/>
+    <Search/>
+  </div>
+  </> 
   )
 }
 
---
+++ src/components/search/Search.jsx
@@ -0,0 +1,9 @@
+import './Search.css'
+
+function Search(){
+    return (
+        <input type="text" placeholder='choose the pokemon' id='search-input'/>
+    )
+}
+
+export default Search
