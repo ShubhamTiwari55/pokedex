5b62ff2a82317a4fa4d41dc063134f0bd719cc11
ShubhamTiwari55 <stofficial059@gmail.com>
2023-09-10 13:57:39 +0530

    debounce added

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,12 +1,14 @@
+import useDebounce from '../../hooks/useDebounce'
 import './Search.css'
 
 function Search({updateSearchTerm}){
+    const debounceUpdateSearch = useDebounce((e)=>updateSearchTerm(e.target.value));
     return (
         <input 
         id='search-input'
         type="text" 
         placeholder='choose the pokemon' 
-        onChange={(e)=>updateSearchTerm(e.target.value)}
+        onChange={debounceUpdateSearch}
         />
     )
 }
